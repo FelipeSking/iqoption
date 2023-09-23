5b9224980a38ce68cd66cbe72a6380c60d6afe85
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-09-23 16:31:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ function calcularEntrada() {
     document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada}`;
 }
 
+//--------------------------------------Botões------------------------------------------------------------------
+
+
 // Função para lidar com um clique no botão "Win"
 function win() {
     valorBanca += (valorEntrada * (payout / 100));
@@ -34,6 +37,18 @@ function loss() {
     atualizarPlacar();
 }
 
+
+// Função para lidar com um clique no botão "Limpar"
+function Limpar() {
+    valorBanca = 0;
+    losses = 8;
+    wins = 0;
+    valorEntrada = 0;
+    atualizarPlacar();
+}
+
+
+//--------------------------PLACAR--------------------------------------------------------------
 // Função para atualizar o placar
 function atualizarPlacar() {
     document.getElementById('wins').textContent = `Wins: ${wins}`;
@@ -41,7 +56,10 @@ function atualizarPlacar() {
     document.getElementById('bancaAtual').textContent = `Valor da Banca: ${valorBanca.toFixed(2)}`;
 }
 
+//--------------------------Adição de botões-----------------------------------------------
+
 // Associar funções aos botões
 document.getElementById('calcularEntrada').addEventListener('click', calcularEntrada);
 document.getElementById('winButton').addEventListener('click', win);
 document.getElementById('lossButton').addEventListener('click', loss);
+document.getElementById('Limpar').addEventListener('click', Limpar);
