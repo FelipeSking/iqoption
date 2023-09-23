740a9447816ff53e481b1db87d2a711dabef21ab
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-09-23 16:17:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Variáveis para armazenar valores
-   // Variáveis globais
-let banca = 0;
-let valorEntrada = 0;
+// Variáveis para armazenar os valores
+let valorBanca = 0;
+let porcentagemEntrada = 0;
 let payout = 0;
-let gerenciamento = "";
+let valorEntrada = 0;
 let wins = 0;
 let losses = 0;
-let lucro = 0;
-let perda = 0;
 
-// Função para calcular o valor da entrada
+// Função para calcular a entrada
 function calcularEntrada() {
     // Obter os valores do formulário
-    banca = parseFloat(document.getElementById("banca").value);
-    const porcentagemEntrada = parseFloat(document.getElementById("porcentagem").value) / 100;
-    const payoutPorcentagem = parseFloat(document.getElementById("payout").value) / 100;
-    gerenciamento = document.getElementById("gerenciamento").value;
+    valorBanca = parseFloat(document.getElementById('banca').value);
+    porcentagemEntrada = parseFloat(document.getElementById('porcentagem').value);
+    payout = parseFloat(document.getElementById('payout').value);
 
     // Calcular o valor da entrada
-    valorEntrada = banca * porcentagemEntrada;
+    valorEntrada = (valorBanca * (porcentagemEntrada / 100)).toFixed(2);
 
-    // Atualizar o placar
-    document.getElementById("bancaAtual").innerHTML = `Valor da Banca: ${banca.toFixed(2)}`;
+    // Exibir o valor da entrada no local designado
+    document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada}`;
 }
 
-// Função para processar o clique no botão "Win"
+// Função para lidar com um clique no botão "Win"
 function win() {
-    // Calcular lucro com base no payout
-    const lucroWin = valorEntrada * (payout / 100);
-
-    // Atualizar lucro, banca e contador de Wins
-    lucro += lucroWin;
-    banca += lucroWin;
+    valorBanca += (valorEntrada * (payout / 100));
     wins++;
-
-    // Atualizar o placar
     atualizarPlacar();
 }
 
-// Função para processar o clique no botão "Loss"
+// Função para lidar com um clique no botão "Loss"
 function loss() {
-    // Atualizar perda e contador de Losses
-    perda += valorEntrada;
+    valorBanca -= valorEntrada;
     losses++;
-
-    // Atualizar o placar
     atualizarPlacar();
-
-    // Atualizar o valor da entrada com base no gerenciamento
-    if (gerenciamento === "Conservador" && losses === 2) {
-        valorEntrada = valorEntrada * 1.2;
-    } else if (gerenciamento === "Moderado" && losses === 2) {
-        valorEntrada = valorEntrada * 1.5;
-    } else if (gerenciamento === "Agressivo" && losses === 2) {
-        valorEntrada = valorEntrada * 1.75;
-    } else if (gerenciamento === "Conservador" && losses === 3) {
-        valorEntrada = perda * 1.2;
-    } else if (gerenciamento === "Moderado" && losses === 3) {
-        valorEntrada = perda * 1.5;
-    } else if (gerenciamento === "Agressivo" && losses === 3) {
-        valorEntrada = perda * 1.75;
-    }
 }
 
 // Função para atualizar o placar
 function atualizarPlacar() {
-    document.getElementById("wins").innerHTML = `${wins}`;
-    document.getElementById("losses").innerHTML = `${losses}`;
-    document.getElementById("bancaAtual").innerHTML = `${banca.toFixed(2)}`;
-    document.getElementById("lucro").innerHTML = `${lucro.toFixed(2)}`;
-    document.getElementById("perda").innerHTML = `${perda.toFixed(2)}`;
+    document.getElementById('wins').textContent = `Wins: ${wins}`;
+    document.getElementById('losses').textContent = `Losses: ${losses}`;
+    document.getElementById('bancaAtual').textContent = `Valor da Banca: ${valorBanca.toFixed(2)}`;
 }
 
-// Adicionar manipuladores de eventos aos botões
-document.getElementById("calcularEntrada").addEventListener("click", calcularEntrada);
-document.getElementById("winButton").addEventListener("click", win);
-document.getElementById("lossButton").addEventListener("click", loss);
+// Associar funções aos botões
+document.getElementById('calcularEntrada').addEventListener('click', calcularEntrada);
+document.getElementById('winButton').addEventListener('click', win);
+document.getElementById('lossButton').addEventListener('click', loss);
