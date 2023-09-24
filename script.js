993fad32a1448cbf1b365b720ae4ec4d54cf2756
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-09-24 10:47:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function atualizarvalorEntrada() {
     let gerenciamento = document.getElementById('gerenciamento').value;
    
     // Atualizar o valor da entrada com base no gerenciamento e nas perdas
-    if (gerenciamento === "Conservador" || nivelloss > 1 ) {
+    if (gerenciamento === "Conservador" || nivelloss > = 1 ) {
         valorEntrada *= 1.2;
          // Exibir o valor da entrada atualizado
     document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada.toFixed(2)}`;
