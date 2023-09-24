ffb0cb440ae4a612c33051f5b01b80c04499d5eb
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-09-24 13:43:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ function win() {
     
     if (nivelwin == 2 ){
         nivelloss = 0;
-      
-    }
+      }
+    
     atualizarPlacar();
     atualizarvalorEntrada();
     atualizarnivelwin();
@@ -105,6 +105,12 @@ function atualizarvalorEntrada() {
     // Atualizar o valor da entrada com base no gerenciamento e nas perdas
     if (gerenciamento === "Conservador" && nivelloss >= 1 ) {
         valorEntrada *= 1.2;
+         // Exibir o valor da entrada atualizado
+    document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada.toFixed(2)}`;
+    } 
+
+    if (gerenciamento === "Conservador" && nivelloss == 0 ) {
+        valorEntrada = entradainicial;
          // Exibir o valor da entrada atualizado
     document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada.toFixed(2)}`;
     } 
