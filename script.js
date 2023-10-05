665b21d3c7ed7e9d09fd1a2260c986eca943c6e8
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-10-05 00:21:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ let losses = 0;
 let gerenciamento = "";
 let nivelwin = 0;
 let nivelloss =0;
-let entradainicial = 0; 
+let entradainicial = 0;
+let perda =0;
+let lucro =0;
+
 
 // Multiplicadores de valor de entrada para cada nível de Loss
-const multiplicadoresEntrada1 = [1, 1.2, 1.2, 1.3, 1.3, 1.4, 1.4, 1.5, 1.5, 1.6];
-const multiplicadoresEntrada2 = [1, 2, 1, 2, 1, 1, 1.75, 2, 2, 2];
-const multiplicadoresEntrada3 = [2, 2, 2, 2, 1.5, 1.75, 1.75, 2, 2, 2];
+const multiplicadoresEntrada1 = [1,1, 1.2, 1.3, 1.2, 1.4, 1.3, 1.4, 1.3, 1.5, 1.4,1.6,1.5,1.7,1.9,1.7,1.9,1.8];
+const multiplicadoresEntrada2 = [1, 1, 1.5, 1.6, 1.4, 1.7, 1.6, 1.8, 1.7, 1.9, 1.8, 2, 1.9, 2.1, 2.3, 2.1, 2.3, 2.2];
+const multiplicadoresEntrada3 = [1, 1, 2, 2.2, 1.8, 2.5, 2.2, 2.8, 2.5, 3, 2.8, 3.5, 3, 3.8, 4, 3.8, 4, 4];
 
 
 
@@ -44,28 +47,44 @@ function Limpar() {
     valorEntrada = 0;
     nivelloss = 0; 
     nivelwin = 0; 
-    atualizarPlacar();
+    perda = 0;
+    lucro = 0;
+
+    
     atualizarnivelwin();
     atualizarnivelloss();
     atualizarvalorEntrada();
+    atualizarPlacar();
+    atualizarlucro ();
+    atualizarperda();
+
 }
 
 
 
 // Função para lidar com um clique no botão "Win"
 function win() {
+
     valorBanca += (valorEntrada * (payout / 100));
     wins++;
     nivelwin++;
-    
+    lucro += (valorEntrada * (payout / 100));
+    perda -= (valorEntrada * (payout / 100));
     if (nivelwin == 2 ){
         nivelloss = 0;
+        valorEntrada = +entradainicial;
       }
+
+
+
+     
     
     atualizarPlacar();
     atualizarnivelwin();
     atualizarnivelloss();
     atualizarvalorEntrada();
+    atualizarlucro ();
+    atualizarperda();
 
    
 }
@@ -77,14 +96,33 @@ function loss() {
     losses++;
     nivelloss++;
     nivelwin = 0;
+    lucro -= valorEntrada;
+    perda += valorEntrada;
 
-    
-    
+  
+
+// Ajustar o valor da entrada com base no nível de Loss e gerenciamento
+if (gerenciamento === 'Conservador' && nivelloss < multiplicadoresEntrada1.length) {
+  valorEntrada *= multiplicadoresEntrada1[nivelloss];
+
+}
+if (gerenciamento === 'Moderado' && nivelloss < multiplicadoresEntrada2.length) {
+  valorEntrada *= multiplicadoresEntrada2[nivelloss];
+
+}
+if (gerenciamento === 'Agressivo' && nivelloss < multiplicadoresEntrada3.length) {
+  valorEntrada *= multiplicadoresEntrada3[nivelloss];
+
+}
+
     atualizarPlacar();
-    atualizarvalorEntrada();
     atualizarnivelwin();
     atualizarnivelloss();
-
+    atualizarvalorEntrada();
+    atualizarlucro ();
+    atualizarperda();
+    
+ 
     }
     //--------Atualizar nivel win---------------------------------
 function atualizarnivelwin() {
@@ -98,22 +136,57 @@ function atualizarnivelloss() {
     document.getElementById('nivelloss').textContent = `Nivel loss: ${nivelloss.toFixed(2)}`;  
 }
 
+//------Atualizar Lucro ---------------------------------------
+function atualizarlucro() {
+  document.getElementById('lucro').textContent = `Lucro/Perda: ${lucro.toFixed(2)}`; 
+  
+  // Adicionar classe 'negativo' se houver perda
+  if (lucro < 0) {
+    document.getElementById('lucro').classList.add('negativo');
+} else {
+    document.getElementById('lucro').classList.remove('negativo');
+}
+
+}
+
+
+
+//------Atualizar perda ---------------------------------------
+
+function atualizarperda() {
+  document.getElementById('perda').textContent = `Lucro/Perda: ${perda.toFixed(2)}`;  
+}
+
 //--------------------Função para limpar o valor da entrada ou atualizar-----------------------
 function atualizarvalorEntrada() {
 
      // Obter os valores do formulário
     let gerenciamento = document.getElementById('gerenciamento').value;
    
-    // Atualizar o valor da entrada com base no gerenciamento e nas perdas
-  if (gerenciamento === 'Conservador' && nivelloss >= 1) {
-    valorEntrada *= multiplicadoresEntrada[nivelloss];
-  } if (gerenciamento === 'Moderado' && nivelloss >= 1) {
+    // Ajustar o valor da entrada com base no nível de Loss e gerenciamento
+    if (gerenciamento === 'Conservador' && nivelloss < multiplicadoresEntrada1.length) {
+      valorEntrada *= multiplicadoresEntrada1[nivelloss];
+
+  
+  } 
+  if (gerenciamento === 'Moderado' && nivelloss < multiplicadoresEntrada2.length) {
     valorEntrada *= multiplicadoresEntrada2[nivelloss];
-  }if (gerenciamento === 'Agressivo' && nivelloss >= 1) {
-    valorEntrada *= multiplicadoresEntrada3[nivelloss];
-  }else {
-    valorEntrada = +entradainicial;
+  
   }
+  if (gerenciamento === 'Agressivo' && nivelloss < multiplicadoresEntrada3.length) {
+    valorEntrada *= multiplicadoresEntrada3[nivelloss];
+  
+  }
+  
+  else if((gerenciamento === 'Conservador' && nivelloss > multiplicadoresEntrada1.length)) {
+      valorEntrada = +entradainicial;
+  }
+  else if((gerenciamento === 'Moderado' && nivelloss > multiplicadoresEntrada2.length)) {
+    valorEntrada = +entradainicial;
+}
+else if((gerenciamento === 'Agressivo' && nivelloss > multiplicadoresEntrada3.length)) {
+  valorEntrada = +entradainicial;
+}
     
  // Exibir o valor da entrada atualizado
     document.getElementById('valorEntrada').textContent = `Valor da Entrada: ${valorEntrada.toFixed(2)}`;
